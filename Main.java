498ad6560aca3499328e0a47eb932db772b0f3a8
Anilton Viganigo Junior <viganigojr@gmail.com>
2020-05-17 00:58:17 -0300

    Realizei as melhorias propostas

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
+		private static final String mensagemOk = "Ok, você está apto a ver esse filme.";
+		private static final String mensagemNOk = "Desculpe, este título não está disponível para a sua idade.";
+		
+		public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 
@@ -12,49 +14,38 @@ public class Main {
 		System.out.println();
 		System.out.println("                   Classificação indicativa                    ");
 		System.out.println("===============================================================");
-		System.out.println("        Para a classificação LIVRE, digite (1).");
-		System.out.println("        Para a classificação PRÉ ADOLESCENTE, digite (2).");
-		System.out.println("        Para a classificação ADOLESCENTE, digite (3).");
-		System.out.println("        Para a classificação MAIOR, digite (4).");
-		System.out.println("        Para sair, digite (0)");
+		System.out.println("        Para a classificação LIVRE.('L')                       ");
+		System.out.println("        Para a classificação PRÉ ADOLESCENTE. ('PA').          ");
+		System.out.println("        Para a classificação ADOLESCENTE. ('AD').              ");
+		System.out.println("        Para a classificação MAIOR. ('MI').                    ");
+		System.out.println("        Para sair, digite 'S'.                                 ");
 		System.out.println("===============================================================");
 		System.out.print("Informe a classificação do filme desejada: ");
 		String classificacao = sc.next();
+		
+		if (classificacao.equals("S")) {
+			System.out.println();
+			System.out.println("Programa encerrado!");
+			System.exit(0);
+		}
 
 		System.out.println();
 
-		if (idade < 12 && classificacao.equals("L")) {
-			System.out.println("OK, você está apto a ver este filme.");
-		} else if (idade < 12 && classificacao.equals("PA")) {
-			System.out.println("Desculpe, este título não está disponível para a sua idade.");
-		} else if (idade < 12 && classificacao.equals("AD") {
-			System.out.println("Desculpe, este título não está disponível para a sua idade.");
-		} else if (idade < 12 && classificacao.equals("MI")) {
-			System.out.println("Desculpe, este título não está disponível para a sua idade.");
-		} else if (idade >= 12 && classificacao.equals("L")) {
-			System.out.println("OK, você está apto a ver este filme.");
-		} else if (idade >= 12 && classificacao.equals("PA")) {
-			System.out.println("OK, você está apto a ver este filme.");
-		} else if (idade < 14 && classificacao.equals("AD") {
-			System.out.println("Desculpe, este título não está disponível para a sua idade.");
-		} else if (idade < 14 && classificacao.equals("MI") {
-			System.out.println("Desculpe, este título não está disponível para a sua idade.");
-		} else if (idade >= 14 && classificacao.equals("L")) {
-			System.out.println("OK, você está apto a ver este filme.");
-		} else if (idade >= 14 && classificacao.equals("PA")) {
-			System.out.println("OK, você está apto a ver este filme.");
-		} else if (idade >= 14 && classificacao.equals("AD")) {
-			System.out.println("OK, você está apto a ver este filme.");
-		} else if (idade < 18 && classificacao.equals("MI")) {
-			System.out.println("Desculpe, este título não está disponível para a sua idade.");
-		} else if (idade >= 18) {
-			System.out.println("OK, você está apto a ver este filme.");
-		} else {
-			System.out.println("===========================================================");
-			System.out.println("Muito obrigado!");
+		Boolean apto = classificacao.equals("L") ||
+			(classificacao.equals("PA") && idade >=12) ||
+			(classificacao.equals("DA") && idade >=14) ||
+			(classificacao.equals("MI") && idade >=18); 
+
+		if (apto == true) {
+			System.out.println(mensagemOk);
+		}
+		else {
+			System.out.println(mensagemNOk);
 		}
 
-		sc.close();
-	}
+		
 
+		sc.close();
+	}  
 }
+
